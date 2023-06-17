ad623ff536525cf9ec876cd83ce3bc17fed05b69
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-06-17 12:55:13 +0300

    Initial Commit

---
+++ FoodieApp/FoodieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieAppApp.swift
+//  FoodieApp
+//
+//  Created by YILDIRIM on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
